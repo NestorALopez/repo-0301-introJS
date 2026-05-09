188faaeda0f30fa31492495470cc32140693585b
Néstor A. López <rohastro.09@gmail.com>
2026-05-09 13:41:46 -0600

    Añadir tipos de datos primitivos y especiales

---
+++ tipos-de-dato.js
@@ -0,0 +1,23 @@
+// Tipos de datos
+
+boolean01 = true;
+boolean02 = false;
+null01 = null;
+number01 = 42;
+number02 = -666;
+number03 = 0;
+string01 = "Saluditos";
+string02 = "Veinticinco";
+string03 = "";
+undefined01 = undefined;
+
+console.log("El tipo de dato de " + boolean01 + " es: " + typeof boolean01);
+console.log("El tipo de dato de " + boolean02 + " es: " + typeof boolean02);
+console.log("El tipo de dato de " + null01 + " es: " + typeof null01);
+console.log("El tipo de dato de " + number01 + " es: " + typeof number01);
+console.log("El tipo de dato de " + number02 + " es: " + typeof number02);
+console.log("El tipo de dato de " + number03 + " es: " + typeof number03);
+console.log("El tipo de dato de " + string01 + " es: " + typeof string01);
+console.log("El tipo de dato de " + string02 + " es: " + typeof string02);
+console.log("El tipo de dato de " + string03 + " es: " + typeof string03);
+console.log("El tipo de dato de " + undefined01 + " es: " + typeof undefined01);
